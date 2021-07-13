ef1fc9b74ac7ffee43dfb49bdeeeab578828f7ed
Evgeniy Ognarev <4496164+ognarev@users.noreply.github.com>
2021-07-13 06:53:04 +0000

    lesson 14 corrections. Format multistring text with "/"

--- lesson_14.py
+++ lesson_14.py
@@ -2,11 +2,12 @@
 
 # Compare texts
 def compare_texts():
-  t1 = """Обратите внимание на порядок элементов результата: на первый взгляд 
-он кажется произвольным. Если для вас важен порядок элементов, вместо 
-множества лучше использовать другой тип данных."""
-  t2 = """Операция объединения (|) возвращает множество, состоящее из всех 
-элементов обоих множеств (с исключением дубликатов)"""
+  t1 = """Обратите внимание на порядок элементов результата: /
+          на первый взгляд он кажется произвольным. Если для /
+          вас важен порядок элементов, вместо множества лучше/ использовать другой тип данных."""
+  t2 = """Операция объединения (|) возвращает множество,/  
+          состоящее из всех элементов обоих множеств (с /
+          исключением дубликатов)"""
   t1l = set(t1.split())
   t2l = set(t2.split())
   words_both = t1l & t2l
@@ -14,4 +15,16 @@ def compare_texts():
   print(f"Words in both texts: {words_both}\n")
   print(f"Unic words in both texts: {words_unic}\n")
 
+# Find unic methods for list and cortage types
+def compare_methods():
+  li = set(dir([]))
+  cort = set(dir((1,)))
+  list_unic = li - cort
+  cort_unic = cort - li
+  text = (f"\
+  This is unic metods for list:\n {list_unic}\n\n\
+  This is unic methods for cortage:\n {cort_unic}")
+  print(text)
+
 compare_texts()
+compare_methods()
