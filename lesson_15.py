e1c041f904fd8832a5327427f4920d3bc0611ac6
Evgeniy Ognarev <4496164+ognarev@users.noreply.github.com>
2021-07-14 09:38:10 +0000

    - Lesson 15. Cycles for.

--- lesson_15.py
+++ lesson_15.py
@@ -1,2 +1,16 @@
-# Cycle for
+# for cycle, else, break, continue
 
+# Calculating average name length from the names list
+def aver_name_lenght():
+  name_list = ['Jim', 'Jack', 'Rickie', 'Mothes', 'Daniels']
+  names_l_sum = 0
+
+  for name_ in name_list:
+    name_l = len(name_)
+    names_l_sum += name_l
+    print(f"{name_} lenght is: {name_l}")
+
+  aver_l = names_l_sum / len(name_list)
+  print(f"\nAverage name lenght in this list is {aver_l}")
+  
+aver_name_lenght()
