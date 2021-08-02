db7c6090c88582a86ac4c74a1f18af4c5c9a39c3
ognarev <ognarev@gmail.com>
2021-08-02 23:35:38 +0300

    lesson 19. csv reader first attempt

--- lesson_19.py
+++ lesson_19.py
@@ -1,4 +1,5 @@
 # Open, read and write files 
+import os
 
 data = [
   ("Buratino", "Strana Durakoff", 10), 
@@ -8,10 +9,29 @@ data = [
   
 def write_to_file(data, file_name):
   "Writes data from list to CSV file"
+  columns = 'name, address, age\n'
   with open(file_name, 'w') as fout:
+    fout.write(columns)
     for item in data:
       text_row = []
-      text_row = f"{data[0]}, {data[1]}, {data[2]}\n"
+      text_row = f"{item[0]}, {item[1]}, {item[2]}\n"
       fout.write(text_row)
       
-write_to_file(data, r"data/contacts.csv")
+def csv_interpreter(csv_file):
+  "Return list of dictionaries. Convert csv file to list of dictionaries"
+  list_of_dicts = []
+  dict_keys = []
+  with open(csv_file, 'r') as fin:
+    for row in fin:
+      #get keys from first row
+      if ~len(list_of_dicts):
+        n = 0
+        while n < len(row):
+          dict_keys.append(row[n])
+          n += 1      
+      #convert all rows to dictionaries
+  return list_of_dicts
+
+#write_to_file(data, r"data/contacts.csv")
+list = csv_interpreter(r"data/contacts.csv")
+print(list)
